bf0cb49e075c1927ecfe409511b6712824f23f05
Edward Zota <zotaedward@gmail.com>
2023-05-22 16:30:58 +0200

    first commit

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i=0;$i<10;$i++){
+            $newProject = new Project();
+            $newProject->title = $faker->sentence(5);
+            $newProject->description = $faker->text(300);
+            $newProject->link = $faker->url();
+            $newProject->preview_image = $faker->url();
+            $newProject->save();
+        }
+    }
+}
